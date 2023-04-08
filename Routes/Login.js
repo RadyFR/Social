26af63d42a817c314f92f62b47e5a4bab770a497
RadyFR <radifr23@gmail.com>
2023-04-08 17:31:22 -0400

    Creando las rutas para el login y el registro

--- Routes/Login.js
+++ Routes/Login.js
@@ -3,5 +3,7 @@ const route  = express.Router();
 const LoginControll = require("../Controller/LoginController")
 
 route.get("/",LoginControll.showLogin);
+route.post("/loggedIn", LoginControll.InSeccion)
+route.post("/register-user", LoginControll.Register)
 
 module.exports = route;
