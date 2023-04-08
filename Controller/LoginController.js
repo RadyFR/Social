287d049d2f46185a0c713d79d9a58b7c0b2cd516
RadyFR <radifr23@gmail.com>
2023-04-08 17:36:27 -0400

    Creando los actions para el login y el registro

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -1,3 +1,24 @@
+const LoginModel = require("../Model/LoginModel");
+
 exports.showLogin = (req,res,next) =>{
     res.render("login/Login",{tittle:"Inicio de seccion"})
+}
+
+exports.InSeccion = (req, res, next) =>{
+    res.setHeader("Set-Cookie", "loggedIn=true; expires=" +new Date(new Date.getTime() + 86409000).toString());
+
+}
+
+
+exports.Register = (req, res, next) =>{
+    const name = req.body.Name;
+    const email = req.body.Email;
+    const pass = req.body.Password;
+
+    LoginModel.create({Name:name, email:email,password:pass}).then(result =>{
+        res.redirect("/")
+    }).catch(err =>{
+        console.log(err);
+    })
+
 }
