ecf65606ea294e7688c9aba96a74fb412af1c5b4
RadyFR <radifr23@gmail.com>
2023-04-10 10:59:45 -0400

    configarando las sessiones ojo: tengo que checar esto para que las sessiones funcionen

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {engine} = require("express-handlebars")
 const path = require("path");
+const session = require("express-session")
 const notfound = require("./Routes/404NotFound")
 const Login = require("./Routes/Login")
 const cone = require("./DB/Coneccion")
@@ -15,6 +16,17 @@ app.set("views", "views")
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
 app.use("/image",express.static(path.join(__dirname,"image")));
+app.use(session({
+    secret:"Login",
+    resave: true,
+    saveUninitialized: true,
+
+}));
+
+app.use((req,res,next) => {
+    req.session.isAutheticate = req.session.loggedIn;
+    next();
+});
 
 app.use(Login)
 app.use(notfound)
