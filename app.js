ef92b428defe0ec8445eff7eb8ba8ccb589c43e2
RadyFR <radifr23@gmail.com>
2023-04-18 09:24:25 -0400

    configuracion de multer

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const {engine} = require("express-handlebars")
+const { v4:uuidv4 } = require("uuid")
+const multer = require("multer")
 const path = require("path");
 const session = require("express-session")
 const notfound = require("./Routes/404NotFound")
@@ -19,12 +21,26 @@ app.use("/image",express.static(path.join(__dirname,"image")));
 app.use(session({
     secret:"Login",
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
 
 }));
 
+const uploadImage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb( null, "image")
+    },
+    filename: function(req,file,cb){
+        const idImage = uuidv4();
+        cb(null,`${idImage}-${file.originalname}`)
+    }
+})
+
+app.use(multer({
+    storage: uploadImage
+}).single("photo"))
+
 app.use((req,res,next) => {
-    req.session.isAutheticate = req.session.loggedIn;
+    res.locals.isAutheticate =  req.session.loggedIn;
     next();
 });
 
